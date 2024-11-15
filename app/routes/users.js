bd14288625b3684ff5c5ddda289434537c24f45f
vlanuka <andreychuk04@mail.ru>
2024-11-16 01:31:36 +0300

    fix(api): route to get users info by id

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,13 +3,13 @@ const userController = require("../controllers/users.js");
 
 const router = express.Router();
 
-router.post("/register", userController.create);
-// router.get("/:id", userController.read);
-router.put("/:id", userController.update);
-router.delete("/:id", userController.remove);
-
 router.post("/login", userController.login);
 router.get("/profile", userController.profile);
 router.post("/logout", userController.logout);
+
+router.post("/register", userController.create);
+router.get("/:id", userController.read);
+router.put("/:id", userController.update);
+router.delete("/:id", userController.remove);
 
 module.exports = router;
